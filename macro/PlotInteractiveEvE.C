8061b4a7b3c78e39686710ee712967018205d260
Guido Fantini <guido.fantini@gssi.it>
2018-06-11 16:32:03 +0200

    small fix

--- macro/PlotInteractiveEvE.C
+++ macro/PlotInteractiveEvE.C
@@ -12,10 +12,12 @@
 
 using namespace std;
 
+const double BaselineSlopeCut = 0.01;
+
 void PlotInteractiveEvE(string filename = "PlotCoincidencesEnergyVsEnergy.root")
 {
   string input = "/nfs/cuore1/scratch/gfantini/spacebased/out/";
-	input += filename;
+  input += filename;
   TFile* pFinput = new TFile(input.c_str(),"UPDATE");
   TTree* outTree;
   gDirectory->GetObject("outTree",outTree);
@@ -34,38 +36,58 @@ void PlotInteractiveEvE(string filename = "PlotCoincidencesEnergyVsEnergy.root")
   outTree->SetBranchAddress("BaselineSlope",&BaselineSlope);
   outTree->SetBranchAddress("Radius",&Radius);
   outTree->SetBranchAddress("Multiplicity",&Multiplicity);
-  gROOT->cd(); // Wil. E. Coyote
+  //gROOT->cd(); // Wil. E. Coyote
   
   const int cNbin = 3000;
   const double cEmin = 0.;
   const double cEmax = 3000.;
 
-  TGraph* gBase = new TGraph();
+  TGraph* gBase = new TGraph(); // plot TotalEnergy - Energy : Energy
   int gBaseCounter = 0;
-  TGraph* g2615sum = new TGraph();
+  TGraph* g2615sum = new TGraph(); // same plot with TotalEnergy ~ 2615 selection
   int g2615sumCounter = 0;
-  TGraph* g2615acc = new TGraph();
+  TGraph* g2615acc = new TGraph(); // same plot with Energy ~ 2615 selection
   int g2615accCounter = 0;
-
   TH1D* h2615sum = new TH1D("h2615sum","h2615sum",1200,0.,1200.);
   TH1D* h2615acc = new TH1D("h2615acc","h2615acc",1200,0.,1200.);
-  //  TH2D* hBase = new TH2D("hBase","hBase",cNbin,cEmin,cEmax,cNbin,cEmin,cEmax);
-  //  outTree->Draw(Form("TotalEnergy - Energy : Energy >>hBase(%d,%lf,%lf,%d,%lf,%lf)",cNbin,cEmin,cEmax,cNbin,cEmin,cEmax),cutBase);
+
+  TGraph* gBaseM2 = new TGraph(); // plot TotalEnergy - Energy : Energy          
+  int gBaseCounterM2 = 0;
+  TGraph* g2615sumM2 = new TGraph(); // same plot with TotalEnergy ~ 2615 selection      
+  int g2615sumCounterM2 = 0;
+  TGraph* g2615accM2 = new TGraph(); // same plot with Energy ~ 2615 selection  
+  int g2615accCounterM2 = 0;
+  TH1D* h2615sumM2 = new TH1D("h2615sumM2","h2615sumM2",1200,0.,1200.);
+  TH1D* h2615accM2 = new TH1D("h2615accM2","h2615accM2",1200,0.,1200.);
+
   for(int i = 0; i < outTree->GetEntries(); i++){
     outTree->GetEntry(i);
     if(i%100000 == 1)cout << 100.*(double)i/(double)outTree->GetEntries() << " % complete." << endl;
-    if(abs(BaselineSlope) < 0.01 && Energy < 3e3 && TotalEnergy - Energy < 3e3){ // select base
+    if(abs(BaselineSlope) < BaselineSlopeCut && Energy < 3e3 && TotalEnergy - Energy < 3e3){ // select base
       gBase->SetPoint(gBaseCounter,Energy,TotalEnergy - Energy);
       gBaseCounter++;
+      if(Multiplicity == 2)gBaseM2->SetPoint(gBaseCounterM2,Energy,TotalEnergy - Energy);
+
       if(abs(TotalEnergy - 2615) < 10){// select 2615sum
 	g2615sum->SetPoint(g2615sumCounter,Energy,TotalEnergy - Energy);
 	g2615sumCounter++;
 	h2615sum->Fill(Radius);
+	if(Multiplicity == 2){
+	  g2615sumM2->SetPoint(g2615sumCounterM2,Energy,TotalEnergy - Energy);
+	  g2615sumCounterM2++;
+	  h2615sumM2->Fill(Radius);
+	}
+	
       }
-      if(abs(Energy - 2615) < 10){// select 2615acc                                                                                                                                       
+      if(abs(Energy - 2615) < 10){// select 2615acc               
 	g2615acc->SetPoint(g2615accCounter,Energy,TotalEnergy - Energy);
 	g2615accCounter++;
 	h2615acc->Fill(Radius);
+	if(Multiplicity == 2){
+	  g2615accM2->SetPoint(g2615accCounterM2,Energy,TotalEnergy - Energy);
+	  g2615accCounterM2++;
+	  h2615accM2->Fill(Radius);
+	}
       }
       
     }
@@ -73,43 +95,32 @@ void PlotInteractiveEvE(string filename = "PlotCoincidencesEnergyVsEnergy.root")
 
   cout <<   h2615sum->GetEntries() << endl;
   for(int i = 0; i < h2615sum->GetNbinsX(); i++)cout << i << "\t: " << h2615sum->GetBinContent(i) << endl;
-  
 
-  //TCanvas* c1 = new TCanvas();
-  //  c1->Divide(2,1);
-  //  c1->cd(1);
-  //  gBase->Draw("AP");
-  //g2615sum->SetMarkerColor(kRed);
-  //g2615sum->Draw("PSAME");
-  //g2615acc->SetMarkerColor(kBlue);
-  //g2615acc->Draw("PSAME");
-  //  c1->cd(2);
-  //TCanvas* c2 = new TCanvas();
-  //c2->cd();
-  //h2615sum->SetLineColor(kRed);
-  //h2615sum->Draw();
+  cout << "Appending output to " << input << endl;
+  if(pFinput->IsZombie())cerr << "Aiuto, uno zombie" << endl;
+  h2615sum->Write("hsumMX");
+  h2615acc->Write("haccMX");
+  gBase->Write("gBaseMX");
+  g2615sum->Write("gsumMX");
+  g2615acc->Write("gaccMX");
 
-  //  h2615acc->SetLineColor(kBlue);
-  //  h2615acc->Draw("SAME");
-
-//  delete pFinput;
-//  cout << "Producing output on /nfs/cuore1/scratch/gfantini/spacebased/out/PlotInteractiveEvE.root" << endl;
-//  TFile* pFout = new TFile("/nfs/cuore1/scratch/gfantini/spacebased/out/PlotInteractiveEvE.root","RECREATE");
-	TFile* pFout = pFinput;
-	cout << "Appending output to " << input << endl;
-  if(pFout->IsZombie())cerr << "Aiuto, uno zombie" << endl;
-
-  h2615sum->Write("hsum");
-  //  h2615acc->Write("hacc");
-  pFout->Write();
-  pFout->Close();
+  h2615sumM2->Write("hsumM2");
+  h2615accM2->Write("haccM2");
+  gBaseM2->Write("gBaseM2");
+  g2615sumM2->Write("gsumM2");
+  g2615accM2->Write("gaccM2");
+  //  pFinput->Write();
+  pFinput->Close();
   
 }
 
 
-int main()
+int main(int argc, char** argv)
 {
-
-  PlotInteractiveEvE();
+  if(argc != 2){
+    cerr << "Abort. Should call ./PlotInteractiveEvE <inputfile>" << endl;
+    return 1;
+  }
+  PlotInteractiveEvE(argv[1]);
   return 0;
 }
